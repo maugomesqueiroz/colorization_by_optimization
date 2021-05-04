3a4d2fbe5c4eda4313efd9872d730190f08d39df
Mauricio Queiroz <mau.gomes@hotmail.com>
2021-05-03 22:20:35 -0300

    Further dev of code. Mouse click now computes weights heatmap, and beggining of sparse algebra

--- colorization.py
+++ colorization.py
@@ -1,5 +1,9 @@
 import cv2
 import numpy as np
+from scipy import sparse
+from scipy.linalg import solve_banded
+from scipy.sparse.linalg import spsolve
+import matplotlib.pyplot as plt
 
 def convert_to_yuv(image, is_gray=False):
     if is_gray:
@@ -38,13 +42,32 @@ def colorize(input_image, marked_image):
     marked_locations, marked_mask = marked_pixels(input_yuv, marked_yuv)
     masked_data = cv2.bitwise_and(marked_image, marked_image, mask=marked_mask)
 
-    w = affinity_sparse_matrix(input_yuv)
+    # sparse_weights, sparse_idx = affinity_sparse_matrix(input_yuv)
+
+    # u = np.zeros((sparse_weights.shape[0]))
+    # v = np.zeros((sparse_weights.shape[0]))
+    # for (x, y) in marked_locations:
+    #     u[sparse_idx[x,y]] = marked_yuv[x,y,1]
+    #     v[sparse_idx[x,y]] = marked_yuv[x,y,2]
+
+    # im_weights = np.zeros(input_yuv.shape[:2])
+    # for x in range(n):
+    #     for y in range(m):
+    #         a,b = sparse_idx[(50,100),(x,y)]
+    #         im_weights[x,y] = sparse_weights[a,b]
+
+
+    print('HERE')
+    print(f'shape: {u.shape}, \n shapesparse: {sparse_weights.shape}')
+
+    # new_vals = sparse.linalg.spsolve(sparse_weights, u)
+    # new_vals = sparse.linalg.lsqr(sparse_weights, u)[0]
 
 
     #TODO make marked matrix (size x 1)
     #TODO compute colored optimization
     #TODO transform optimization into image again
-
+    # new_vals = linalg.spsolve(A, b)
 
     return masked_data
 
@@ -80,12 +103,16 @@ def affinity_around(i,j, intensity):
     std = intensity_window.std()
     mean = intensity_window.mean()
 
-    affinity_window_matrix = np.zeros(shape=(3,3)) #wrs for Neighborhood
-    yr = window[1,1]
+    affinity_window_matrix = np.zeros(shape=(3,3), dtype=np.float64) #wrs for Neighborhood
+
+    yr = intensity_window[1,1]/255 #center point intensity
     for i in range(window_size):
         for j in range(window_size):
-            ys = intensity_window[i,j]
-            affinity_window_matrix[i,j] = _affinity_function(yr,ys,std)
+            try:
+                ys = intensity_window[i,j]/255
+                affinity_window_matrix[i,j] = _affinity_function(yr,ys,std)
+            except IndexError:
+                affinity_window_matrix[i,j] = 0.0
     return affinity_window_matrix
 
 def window(point, height, width, size=1):
@@ -111,27 +138,74 @@ def affinity_sparse_matrix(yuv):
 
     n = Y.shape[0]
     m = Y.shape[1]
+    size = m*n
 
     sparse_idx = SparseIndex(n,m)
-
-    #TODO define sparce matrix
+    sparce_weights = sparse.lil_matrix((size, size))
 
     for row in range(n):
         for col in range(m):
 
             center_point = row, col
-            center_idx = sparse_idx(center_point)
+            center_idx = sparse_idx[center_point]
 
             affinity_matrix = affinity_around(row,col,Y)
             affinity_window = window((row,col), n, m)
-            affinity_idx = sparce_idx[affinity_window]
+            affinity_idx = sparse_idx[affinity_window]
 
             for k in range(affinity_idx.shape[0]):
                 for l in range(affinity_idx.shape[1]):
-                    sparce_weights[center,affinity_idx[k,l]] = affinity_matrix[k,l]
+                    if k != l: # when k==l, w=1 (the affinity of center with itself)
+                        sparce_weights[center_idx,affinity_idx[k,l]] = -affinity_matrix[k,l]
 
-    return sparce_weights
+            if col>10 and col <15 and row >10 and row <15:
+                print(affinity_matrix)
+                print(affinity_idx, affinity_window)
 
+    return sparce_weights, sparse_idx
+
+def marked_places(input_image, marked_image):
+    ''' Main process of colorization
+    '''
+
+    n = input_image.shape[0]
+    m = input_image.shape[1]
+    size = n*m
+
+    input_yuv = convert_to_yuv(input_image, is_gray=True)
+    marked_yuv = convert_to_yuv(marked_image)
+
+    marked_locations, marked_mask = marked_pixels(input_yuv, marked_yuv)
+    masked_data = cv2.bitwise_and(marked_image, marked_image, mask=marked_mask)
+    return masked_data
+
+def weightmap(point, input_image):
+    ''' Produces a heat map of weights around point.
+    '''
+    input_yuv = convert_to_yuv(input_image, is_gray=True)
+
+    x,y = point
+
+    n = input_image.shape[0]
+    m = input_image.shape[1]
+
+    Y = input_yuv[:,:,0] # First channel only
+    height, width = Y.shape
+
+    std = Y.std()
+    mean = Y.mean()
+
+    affinity_window_matrix = np.zeros(shape=(height,width), dtype=np.float64) #wrs for Neighborhood
+
+    yr = Y[y,x]/255 #center point intensity
+
+    for i in range(n):
+        for j in range(m):
+            ys = Y[i,j]/255
+            affinity_window_matrix[i,j] = _affinity_function(yr,ys,std)
+
+    plt.matshow(affinity_window_matrix)
+    plt.show()
 
 class SparseIndex:
 
@@ -139,7 +213,7 @@ class SparseIndex:
         self.n = n
         self.m = m
         self.size = n*m
-        self._idx = np.arange(size).reshape(n, m)
+        self._idx = np.arange(self.size).reshape(n, m)
 
     def __getitem__(self, key):
 
@@ -152,9 +226,9 @@ class SparseIndex:
             indices = []
             for i in x_vals:
                 for j in y_vals:
-                    indices.append(_idx[i,j])
+                    indices.append(self._idx[i,j])
 
             return np.array(indices).reshape(len(x_vals),len(y_vals))
         else:
-            return self._idx[x,y
+            return self._idx[x,y]
 
--- run.py
+++ run.py
@@ -5,12 +5,20 @@ from colorization import (
     colorize,
     convert_to_rgb,
     convert_to_yuv,
-    convert_to_gray
+    convert_to_gray,
+    weightmap,
+    marked_places
 )
 
 def show_in_output(image):
      cv2.namedWindow("Output")
      cv2.imshow("Output", image)
+
+def mouse_handler(event, x, y, flags, params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print("Clicked at: ",x, y)
+        weightmap((x,y), input_image)
+
 
 input_image = cv2.imread(sys.argv[1])
 marked_image = cv2.imread(sys.argv[2])
@@ -20,10 +28,12 @@ cv2.namedWindow("Marked")
 
 input_image = convert_to_gray(input_image)
 output_image = marked_image
+
 while True:
 
     cv2.imshow("Input", input_image)
     cv2.imshow("Marked", marked_image)
+    cv2.setMouseCallback("Input", mouse_handler)
 
     key = cv2.waitKey(1)
 
@@ -40,6 +50,11 @@ while True:
     if key == ord('y'):
         print('Converting RGB->YUV')
         output_image = convert_to_yuv(output_image)
+        show_in_output(output_image)
+
+    if key == ord('m'):
+        print('Showing marked places!')
+        output_image = marked_places(input_image, marked_image)
         show_in_output(output_image)
 
     if key == ord('r'):
